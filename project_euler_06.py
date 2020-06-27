a1f1ef74f4482fb8e05877feb3be56dd2d6d5a7a
utkazu <hiroshimazeal@gmail.com>
2020-06-27 15:47:10 +0900

    [finish]Problem6完了

---
+++ project_euler_06.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+'''
+Problem6 「二乗和の差」
+
+最初の10個の自然数について, その二乗の和は,
+        12 + 22 + ... + 102 = 385
+最初の10個の自然数について, その和の二乗は,
+        (1 + 2 + ... + 10)2 = 3025
+これらの数の差は 3025 - 385 = 2640 となる.
+同様にして, 最初の100個の自然数について二乗の和と和の二乗の差を求めよ.
+'''
+
+if __name__ == '__main__':
+    numbers = list(range(1, 101))
+
+    summation_of_square = sum(map(lambda x: x ** 2, numbers))
+    square_of_summation = sum(numbers) ** 2
+
+    diff = square_of_summation - summation_of_square
+
+    # answer 25164150
+    print(diff)
