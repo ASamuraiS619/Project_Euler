bdadfd807f5d34292982a0993edad08415a111c2
utkazu <hiroshimazeal@gmail.com>
2020-06-28 16:15:57 +0900

    [finish]Problem7完了

---
+++ project_euler_07.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+'''
+Problem7 「10001番目の素数」
+
+素数を小さい方から6つ並べると 2, 3, 5, 7, 11, 13 であり, 6番目の素数は 13 である.
+10 001 番目の素数を求めよ.
+'''
+
+def is_prime(target, primes):
+    for i in primes:
+        if target % i == 0:
+            return False
+    return True
+
+if __name__ == '__main__':
+    prime_numbers = [2]
+    target = 3
+    while True:
+        if is_prime(target, prime_numbers):
+            prime_numbers.append(target)
+            if len(prime_numbers) == 10001:
+                print(target)   # answer 104743
+                break
+        target += 2
