b3bbd58c1bc94d8c900b4f67712e5e0148c0cafd
utkazu <hiroshimazeal@gmail.com>
2020-07-01 12:51:44 +0900

    [update]変数名を修正し、コメントを追加。

--- project_euler_10.py
+++ project_euler_10.py
@@ -11,22 +11,25 @@ import time
 
 #エラトステネスの篩でnumber以下の素数を列挙
 def primes_up_to(number):
+    # 検査の上限値
     border_line = int(number ** 0.5)
     prime_numbers = [2]
     target_list = list(range(3, number + 1, 2))
 
     while True:
-        candidate = min(target_list)
-        if candidate > border_line:
+        mesh_number = min(target_list)
+        # 検査がborder_lineを超えると残りは全て素数なのでprime_numbersへ。
+        if mesh_number > border_line:
             prime_numbers.extend(target_list)
             break
-        prime_numbers.append(candidate)
+        prime_numbers.append(mesh_number)
 
-        tmp = []
+        # mesh_numberで篩いにかける。割り切れなかったものを次の候補へ
+        tmp_target_list = []
         for i in target_list:
-            if i % candidate != 0:
-                tmp.append(i)
-        target_list = tmp
+            if i % mesh_number != 0:
+                tmp_target_list.append(i)
+        target_list = tmp_target_list
 
     return prime_numbers
 
