0e0e545859e11b0ff2a12325f5fc33633137b847
ASamuraiS619 <asamurais619@gmail.com>
2020-07-28 11:18:23 +0900

    [update]リスト内包表記に修正。処理時間計測追加。

--- project_euler_01.py
+++ project_euler_01.py
@@ -7,12 +7,14 @@ Problem1 「3と5の倍数」
 同じようにして, 1000 未満の 3 か 5 の倍数になっている数字の合計を求めよ.
 '''
 
+import time
 
-proper_numbers = []
+if __name__ == '__main__':
+    start = time.time()
+    proper_numbers = [num for num in range(1, 1000) if num % 3 == 0 or num % 5 == 0]
 
-for i in range(1, 1000):
-    if i % 3 == 0 or i % 5 == 0:
-        proper_numbers.append(i)
+    # answer 233168
+    print(sum(proper_numbers))
 
-# answer 233168
-print(sum(proper_numbers))
+    elapsed_time = time.time() - start
+    print("elapsed_time:{}".format(round(elapsed_time, 5)) + "[sec]")   # 0.00014sec
