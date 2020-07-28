590f8167bcd317175050b9c986baf70e7a3178fb
ASamuraiS619 <asamurais619@gmail.com>
2020-07-28 11:59:13 +0900

    [update]処理時間の計測を追加。

--- project_euler_07.py
+++ project_euler_07.py
@@ -6,16 +6,18 @@ Problem7 「10001番目の素数」
 素数を小さい方から6つ並べると 2, 3, 5, 7, 11, 13 であり, 6番目の素数は 13 である.
 10 001 番目の素数を求めよ.
 '''
-
+import time
 from tqdm import tqdm
 
 def is_prime(target, primes):
-    for i in primes:
-        if target % i == 0:
+    for prime in primes:
+        if target % prime == 0:
             return False
     return True
 
 if __name__ == '__main__':
+    start = time.time()
+
     prime_numbers = [2]
     target = 3
     with tqdm(total = 10000) as pbar:
@@ -27,3 +29,6 @@ if __name__ == '__main__':
                     print(target)   # answer 104743
                     break
             target += 2
+
+    elapsed_time = time.time() - start
+    print("elapsed_time:{}".format(round(elapsed_time, 5)) + "[sec]")   # 2.33879sec
