aba2e68c8f7643edcda8803ba9158393c971cea4
ASamuraiS619 <asamurais619@gmail.com>
2020-07-28 11:52:49 +0900

    [update]素数を列挙しない、より高速な方法に修正。

--- project_euler_03.py
+++ project_euler_03.py
@@ -9,39 +9,18 @@ Problem3 「最大の素因数」
 
 import time
 
-#エラトステネスの篩でnumber以下の素数を列挙
-def primes_up_to(number):
-    border_line = int(number ** 0.5)
-    prime_numbers = [2]
-    target_list = list(range(3, number + 1, 2))
-
-    while True:
-        candidate = min(target_list)
-        if candidate > border_line:
-            prime_numbers.extend(target_list)
-            break
-        prime_numbers.append(candidate)
-
-        tmp = []
-        for i in target_list:
-            if i % candidate != 0:
-                tmp.append(i)
-        target_list = tmp
-
-    return prime_numbers
-
 if __name__ == '__main__':
     start = time.time()
 
-    #素因数は最大でも√600851475143以下なので、そこまでの素数で逆順に割っていき、最初に割り切れたのが答え。
-    border_line_for_check = int(600851475143 ** 0.5)
-    target_primes = sorted(primes_up_to(border_line_for_check), reverse = True)
+    target = 600851475143
+    THRESHOLD = int(target ** 0.5)    # 775146
+    for num in range(3, THRESHOLD, 2):
+        if target == num:
+            answer = num
+        while target % num == 0:
+            target /= num
 
-    for i in target_primes:
-        if 600851475143 % i == 0:
-            # answer 6857
-            print(i)
-            break
+    print(answer)   # answer 6857
 
     elapsed_time = time.time() - start
-    print("elapsed_time:{}".format(round(elapsed_time, 3)) + "[sec]")   # 1.31sec
+    print("elapsed_time:{}".format(round(elapsed_time, 5)) + "[sec]")   # 0.06638sec
