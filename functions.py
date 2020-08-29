fbbf928b30e4bf9e02993325b5f9d72065bdca11
ASamuraiS619 <asamurais619@gmail.com>
2020-08-29 21:35:18 +0900

    [update]primes_up_to関数を集合を活用した方法に修正

--- functions.py
+++ functions.py
@@ -1,30 +1,15 @@
 def primes_up_to(number):
-    if number < 2:
-        prime_numbers = None
-    elif number <= 4 :
-        prime_numbers = [2]
-    else:
-        # 検査の上限値
-        border_line = int(number ** 0.5)
-        prime_numbers = [2]
-        target_list = list(range(3, number + 1, 2))
-
-        while True:
-            mesh_number = min(target_list)
-            # 検査がborder_lineを超えると残りは全て素数なのでprime_numbersへ。
-            if mesh_number > border_line:
-                prime_numbers.extend(target_list)
-                break
-            prime_numbers.append(mesh_number)
-
-            # mesh_numberで篩いにかける。割り切れなかったものを次の候補へ
-            tmp_target_list = []
-            for i in target_list:
-                if i % mesh_number != 0:
-                    tmp_target_list.append(i)
-            target_list = tmp_target_list
-
-        return prime_numbers
+    primes = set([i for i in range(3, number, 2)])
+    tmp = set()
+    while True:
+        prime = min(primes)
+        tmp.add(prime)
+        if prime > number ** 0.5:
+            primes = primes | tmp
+            primes.add(2)
+            return sorted(list(primes))
+        else:
+            primes = primes - set([i for i in range(prime, number, prime)])
 
 def is_prime(num):
     if num <= 1:
