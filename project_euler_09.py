a3a5e44a71f18791c29d71e694249f8a05165d99
ASamuraiS619 <asamurais619@gmail.com>
2020-08-31 23:32:37 +0900

    [update]処理時間計測とただ1組の答えを見つけたらfor文を抜ける様修正

--- project_euler_09.py
+++ project_euler_09.py
@@ -12,9 +12,22 @@ a + b + c = 1000 となるピタゴラスの三つ組が一つだけ存在する
 これらの積 abc を計算しなさい.
 '''
 
+import time
+
 if __name__ == '__main__':
-    for a in range(1, int(1000/3)):
-        for b in range(a+1, int((1000-a)/2)):
+    start = time.time()
+
+    # a < b < cとして考える。
+    flag = False
+    for a in range(3, 1000 // 3 + 1):
+        for b in range(a+1, (1000-a) //2 + 1):
             c = 1000 - a - b
             if a ** 2 + b ** 2 == c ** 2:
                 print(a * b * c)    # answer 31875000
+                flag = True
+                break
+        if flag == True:
+            break
+
+    elapsed_time = time.time() - start
+    print("elapsed_time:{}".format(round(elapsed_time, 5)) + "[sec]")   # 0.06115sec
